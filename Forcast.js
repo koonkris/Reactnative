e0bac12623c3ff6a234765bb0c9182dbbc0c45fa
KRISNAN ARMA <krisnanddd@gmail.com>
2022-08-27 01:07:04 +0700

    part2.2

---
+++ Forcast.js
@@ -0,0 +1,13 @@
+export default function Forecast(props) {
+    return (
+    <View >
+        <Text>{props.main}</Text>
+        <Text>{props.description}</Text>
+        <View>
+            <Text>{props.temp}</Text>
+            <Text>°C</Text>
+        </View>
+    </View>
+    );
+}
+   
