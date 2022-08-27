9aebdcef7df563fbd12dd1fe0456956a2e7e9456
KRISNAN ARMA <krisnanddd@gmail.com>
2022-08-27 21:35:40 +0700

    part5

---
+++ screen/WeatherScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function WeatherScreen({route}) {
+    return (
+    <View>
+    <Weather zipCode={route.params.zipCode} />
+    <StatusBar style="auto" />
+    </View>
+    );
+   }
---
+++ screen/ZipCodeScreen.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { FlatList, View, Text, StyleSheet} from "react-native";
+import { TouchableHighlight } from "react-native-getsure-hadler";
+import { useNavigation } from '@react-navigation/native'
+
+    const availableZipItems = [
+        { place: 'Hatyai', code: '90110' },
+        { place: 'Trang', code: '92000' },
+        { place: 'Chiangmai', code: '50000' },
+        { place: 'Khonkaen', code: '40000' },
+        { place: 'Chonburi', code: '20000' },
+    ]
+
+    const ZipItem = ({place, code, navigation}) => (
+        <TouchableHighlight onPress={() => navigation.navigate('Weather', { zipCode: code})}>
+            <View>
+                <Text>{place}</Text>
+                <Text >{code}</Text>
+            </View>
+        </TouchableHighlight>
+    )
+   
+export default function ZipCodeScreen(){
+    const navigation = useNavigation()
+    return (
+    <View>
+        <FlatList
+            data={availableZipItems}
+            keyExtractor={_keyExtractor}
+            renderItem={({item}) => <ZipItem {...item} navigation={navigation}/>}
+        />
+        <StatusBar style="auto" />
+    </View>
+    );
+    
+}
